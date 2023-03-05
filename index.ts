dcea83ea248cddea81a11408905ac729fd6fab6f
MigueldCast <miguel.acastillodiaz@outlook.es>
2023-03-04 23:54:47 -0500

    testing methods on index.ts

--- index.ts
+++ index.ts
@@ -1,21 +1,24 @@
 import * as dotenv from "dotenv";
 import GithubAuth from "./GithubAuth";
+import { ReposList } from "./types";
 
 //Importing environment variables
 dotenv.config();
 
 async function main() {
-  //Creating instance of GithubAuth class
+  //Creating instance of GithubAuth class (with auth token)
   let Auth = new GithubAuth(process.env.GITHUB_TOKEN as string);
 
-  //Calling getReposList method
-  let response = await GithubAuth.getReposList(Auth);
+  //Calling getReposList method (returns an array of ReposList or an Error)
+  let response: ReposList[] | Error = await GithubAuth.getReposList(Auth);
+
+  //Checking if response is an Error or not and returning accordingly
   if (response instanceof Error) {
     console.log("Error occurred");
     console.log(response);
     return;
   } else {
-    console.log(response.data[16]);
+    console.log(response);
   }
 }
 
